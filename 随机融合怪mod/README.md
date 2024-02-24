fcec4f324966a24fcd4e9f6fdc63e15cc011926a
eternitylarva1 <128346458+eternitylarva1@users.noreply.github.com>
2024-02-24 12:12:41 +0800

    Add files via upload

---
+++ 随机融合怪mod/README.md
@@ -0,0 +1,7 @@
+# 随机融合怪mod
+# 作者b站 蝴蝶是幼虫
+* It‘s my first mod.It contains new cards and a new relic.
+* Only support Simplified Chinese.
+* Have fun!
+* If you have any question, just report it to me.
+
---
+++ 随机融合怪mod/src/main/java/cn/candy/relic/Spawn_Monster_action.java
@@ -0,0 +1,38 @@
+package cn.candy.relic;
+
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.actions.common.RelicAboveCreatureAction;
+import com.megacrit.cardcrawl.actions.common.SpawnMonsterAction;
+import com.megacrit.cardcrawl.actions.common.SuicideAction;
+import com.megacrit.cardcrawl.actions.unique.CanLoseAction;
+import com.megacrit.cardcrawl.actions.unique.CannotLoseAction;
+import com.megacrit.cardcrawl.actions.utility.HideHealthBarAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.core.Settings;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.powers.AbstractPower;
+import com.megacrit.cardcrawl.powers.VulnerablePower;
+import com.megacrit.cardcrawl.powers.WeakPower;
+import com.megacrit.cardcrawl.vfx.cardManip.ShowCardAndObtainEffect;
+
+import java.util.Iterator;
+import java.util.Random;
+
+
+public class Spawn_Monster_action extends AbstractGameAction {
+    AbstractMonster m;
+    AbstractMonster SP;
+    public Spawn_Monster_action(Iterator var2) {
+
+
+        }
+
+
+
+
+    public void update() {
+
+    }
+}
